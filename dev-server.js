fd93d60036d3a4556974241ff3cdfb2f06359d42
Michael D. Norman <michael.norman@denormans.com>
2018-09-06 22:48:06 -0500

    allow environment variable for server port

--- dev-server.js
+++ dev-server.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 
+const WEBPACK_SERVER_PORT = process.env.WEBPACK_SERVER_PORT || 5000;
+
 const config = require('./webpack.config')({}, {});
 const options = {
   contentBase: './dist',
@@ -14,6 +16,6 @@ WebpackDevServer.addDevServerEntrypoints(config, options);
 const compiler = webpack({ ...config, mode: 'development' });
 const server = new WebpackDevServer(compiler, options);
 
-server.listen(5000, 'localhost', () => {
-  console.log('dev server listening on port 5000');
+server.listen(WEBPACK_SERVER_PORT, 'localhost', () => {
+  console.log(`dev server listening on port ${WEBPACK_SERVER_PORT}`);
 });
